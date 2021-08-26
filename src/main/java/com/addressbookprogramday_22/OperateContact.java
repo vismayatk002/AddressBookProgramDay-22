adb52d3f10eafa6d84da4ac50775f631237f8111
vismayatk002 <vismayatk002@gmail.com>
2021-08-26 18:11:30 +0530

    Refactor to delete given contact

--- src/main/java/com/addressbookprogramday_22/OperateContact.java
+++ src/main/java/com/addressbookprogramday_22/OperateContact.java
@@ -18,8 +18,25 @@ public class OperateContact{
         else{
             System.out.print("\ncouldn't find the contact..");
         }
-        sc.close();
     }
+    
+    public AddressBook deleteContact(AddressBook contact){
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("\nEnter the First name for delete : ");
+        String deleteName = sc.nextLine();
+        if(deleteName.equals(contact.getFirstName())){
+            AddressBook detail = new AddressBook();
+            System.out.print("\nYour contact deleted..");
+            return detail;
+        }
+        else{
+            System.out.print("\ncouldn't find the contact..");
+        }
+        sc.close();
+        return contact;        
+    }
+    
     public void showContact(AddressBook contact){
 
         System.out.print("\n-----------------");
